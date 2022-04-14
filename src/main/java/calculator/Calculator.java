412499f4c1478e3bcfe6e9ca8ee8183c62b1838c
dheer009 <99137614+dheer009@users.noreply.github.com>
2022-04-14 19:29:52 +0530

    Update Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -77,7 +77,7 @@ public class Calculator {
         return result;
     }
 
-
+    //hello java
 
     public double sqroot(double number1) {
         logger.info("[SQ ROOT] - " + number1);
@@ -118,5 +118,5 @@ public class Calculator {
         { facto *= i;   }
         return  facto;
     }
-    //hello
+    
 }
